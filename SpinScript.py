710a8173083c48e2f0fd71730eb35e2839c92edc
Seva167 <90449186+Seva167@users.noreply.github.com>
2021-11-06 23:28:46 +0700

    Add files via upload

---
+++ SpinScript.py
@@ -0,0 +1,39 @@
+import win32api, win32con, keyboard, time, math, random
+
+print('Press Q to quit')
+rottype = input('Enter rotation type (available types: rot, sin, tan, sinabs, rand): ')
+delay = (float)(input('Enter delay before spinning: '))
+
+if not rottype == 'rand':
+    multiplier = (float)(input('Set speed: '))
+    print(f'You have {delay} seconds... Wheeee!')
+    time.sleep(delay)
+
+if rottype == 'rot':
+    while not keyboard.is_pressed('q'):
+        mouseX = (int)(time.time()*0.000000001* multiplier)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+        time.sleep(0.01)
+elif rottype == 'sin':
+    while not keyboard.is_pressed('q'):
+        mouseX = (int)(math.sin(time.time())* multiplier)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+        time.sleep(0.01)
+elif rottype == 'tan':
+    while not keyboard.is_pressed('q'):
+        mouseX = (int)(math.tan(time.time())* multiplier)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+        time.sleep(0.01)
+elif rottype == 'sinabs':
+    while not keyboard.is_pressed('q'):
+        mouseX = (int)(math.fabs(math.sin(time.time())* multiplier))
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+        time.sleep(0.01)
+elif rottype == 'rand':
+    maxval = (int)(input('Enter maximum possible random value: '))
+    print(f'You have {delay} second... Wheeee!')
+    time.sleep(delay)
+    while not keyboard.is_pressed('q'):
+        mouseX = (int)(random.randint(0,maxval))
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+        time.sleep(0.1)
