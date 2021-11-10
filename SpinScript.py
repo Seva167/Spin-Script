586edba68eeaf7d2011ba81e90fa2452ce3f6131
Seva167 <90449186+Seva167@users.noreply.github.com>
2021-11-10 21:29:44 +0700

    Update SpinScript.py

--- SpinScript.py
+++ SpinScript.py
@@ -2,7 +2,10 @@ import win32api, win32con, keyboard, time, math, random
 
 print('Press Q to quit')
 rottype = input('Select rotation type (available types: rot, sin, tan, sinabs, rand): ')
+
 delay = (float)(input('Enter starting delay: '))
+
+pause = False
 
 if not rottype == 'rand':
     multiplier = (float)(input('Set speed: '))
@@ -11,29 +14,50 @@ if not rottype == 'rand':
 
 if rottype == 'rot':
     while not keyboard.is_pressed('q'):
-        mouseX = (int)(time.time()*0.000000001* multiplier)
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
-        time.sleep(0.01)
+        if not pause:
+            mouseX = (int)(time.time()*0.000000001* multiplier)
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+            time.sleep(0.01)
+        if keyboard.is_pressed('p'):
+            time.sleep(0.1)
+            pause = not pause
 elif rottype == 'sin':
     while not keyboard.is_pressed('q'):
-        mouseX = (int)(math.sin(time.time())* multiplier)
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
-        time.sleep(0.01)
+        if not pause:
+            mouseX = (int)(math.sin(time.time())* multiplier)
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+            time.sleep(0.01)
+        if keyboard.is_pressed('p'):
+            time.sleep(0.1)
+            pause = not pause
 elif rottype == 'tan':
     while not keyboard.is_pressed('q'):
-        mouseX = (int)(math.tan(time.time())* multiplier)
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
-        time.sleep(0.01)
+        if not pause:
+            mouseX = (int)(math.tan(time.time())* multiplier)
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+            time.sleep(0.01)
+        if keyboard.is_pressed('p'):
+            time.sleep(0.1)
+            pause = not pause
 elif rottype == 'sinabs':
     while not keyboard.is_pressed('q'):
-        mouseX = (int)(math.fabs(math.sin(time.time())* multiplier))
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
-        time.sleep(0.01)
+        if not pause:
+            mouseX = (int)(math.fabs(math.sin(time.time())* multiplier))
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+            time.sleep(0.01)
+        if keyboard.is_pressed('p'):
+            time.sleep(0.1)
+            pause = not pause
 elif rottype == 'rand':
     maxval = (int)(input('Enter maximum possible random value: '))
-    print(f'You have {delay} seconds to alt-tab... Wheeee!')
+    randdelay = (float)(input('Enter delay between values: '))
+    print(f'You have {delay} second to alt-tab... Wheeee!')
     time.sleep(delay)
     while not keyboard.is_pressed('q'):
-        mouseX = (int)(random.randint(0,maxval))
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
-        time.sleep(0.1)
+        if not pause:
+            mouseX = (int)(random.randint(0,maxval))
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, mouseX, 0, 0, 0)
+            time.sleep(randdelay)
+        if keyboard.is_pressed('p'):
+            time.sleep(0.1)
+            pause = not pause
